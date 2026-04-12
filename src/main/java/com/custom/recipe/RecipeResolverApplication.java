bb1dab4b977a940bf61d82690b763893052fb840
testName <test@example.com>
2026-04-13 00:25:43 +0900

    project init

---
+++ src/main/java/com/custom/recipe/RecipeResolverApplication.java
@@ -0,0 +1,13 @@
+package com.custom.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeResolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeResolverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/custom/recipe/RecipeResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.custom.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeResolverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
